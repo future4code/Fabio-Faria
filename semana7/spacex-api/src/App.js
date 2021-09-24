96331db1358e033d5d4b1b030b24bbd7443a2541
Fabio Faria <fabiofadev87@gmail.com>
2021-09-24 00:20:35 -0300

    Estilizado

--- semana7/spacex-api/src/App.js
+++ semana7/spacex-api/src/App.js
@@ -1,5 +1,30 @@
 import React from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const GeneralStyling = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #031926;
+  color: #f4e9cd;
+  box-sizing: border-box;
+
+  h1 {
+    margin-left: 30%;
+  }
+  a {
+    color: #ebe374;
+    text-decoration: none;
+  }
+`;
+
+const MissionContainers = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 4px;
+`;
 
 class App extends React.Component {
   state = {
@@ -22,19 +47,19 @@ class App extends React.Component {
   render() {
     const missionList = this.state.missions.map((mission) => {
       return (
-        <div key={mission.key}>
-          <p>Mission: {mission.name}</p>
+        <MissionContainers key={mission.mission_id}>
+          <p>Mission: {mission.mission_name}</p>
           <p>Details: {mission.description}</p>
           <a href={mission.twitter}>Link para Twitter</a>
           <hr />
-        </div>
+        </MissionContainers>
       );
     });
     return (
-      <>
+      <GeneralStyling>
         <h1>SpaceX Missions API Rendered</h1>
         {missionList}
-      </>
+      </GeneralStyling>
     );
   }
 }
