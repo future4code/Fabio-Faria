30e6009857f44249a6ee0fffaa893faff3d9789e
fabiofa87 <fabiofadev87@gmail.com>
2021-11-19 17:38:00 -0300

    Exercicio aula 42 semana 15

---
+++ semana15/APIRest-Aula44/src/index.ts
@@ -0,0 +1,76 @@
+import express, { Express, Request, Response } from 'express';
+import { AddressInfo } from 'net';
+import cors from 'cors';
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+})
+
+let users = [
+    {
+        id: 1,
+        name: "Alice",
+        email: "alice@email.com",
+        type: "ADMIN",
+        age: 12
+    },
+    {
+        id: 2,
+        name: "Bob",
+        email: "bob@email.com",
+        type: "NORMAL",
+        age: 36
+    },
+    {
+        id: 3,
+        name: "Coragem",
+        email: "coragem@email.com",
+        type: "NORMAL",
+        age: 21
+    },
+    {
+        id: 4,
+        name: "Dory",
+        email: "dory@email.com",
+        type: "NORMAL",
+        age: 17
+    },
+    {
+        id: 5,
+        name: "Elsa",
+        email: "elsa@email.com",
+        type: "ADMIN",
+        age: 17
+    },
+    {
+        id: 6,
+        name: "Fred",
+        email: "fred@email.com",
+        type: "ADMIN",
+        age: 60
+    }
+]
+
+//a - Metodo para buscar todos: get
+//b - Entidade: /users
+
+//Ex2
+
+app.get('/users/:type', (req: Request, res: Response) => {
+    const type = req.params.type;
+    const result = users.filter(user => user.type === type);
+    res.status(200).send(result);
+})
+
+//a /users/:type - utilizei para que a requisicao seja especifica para o type
+//b 
