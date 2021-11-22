f41161df2983b9f53fdf292804aebd013e22d670
fabiofa87 <fabiofadev87@gmail.com>
2021-11-22 17:32:46 -0300

    Exercicio aula 44

--- semana15/APIRest-Aula44/src/index.ts
+++ semana15/APIRest-Aula44/src/index.ts
@@ -73,4 +73,67 @@ app.get('/users/:type', (req: Request, res: Response) => {
 })
 
 //a /users/:type - utilizei para que a requisicao seja especifica para o type
-//b 
+//b Enteidade, no caso, /users
+
+// Ex2
+
+enum UserTypes {
+    NORMAL = "NORMAL",
+    ADMIN = "ADMIN"
+}
+
+type user = {
+    id: number,
+    name: string,
+    email: string,
+    type: UserTypes,
+    age: number
+}
+
+// Usando ENUM podemos especificar o tipo de dados que queremos
+
+// Ex3
+
+app.get('/users/:name', (req: Request, res: Response) => {
+    const name = req.params.name;
+    const result = users.filter(user => user.name === name);
+    if(result.length > 0) {
+        res.status(200).send(result);
+    } else {
+        res.status(404).send({message: "User not found"});
+    }
+})
+
+// Tipo get, para buscar nome usando parametro.
+
+//Ex4
+
+app.put('/users', (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    
+    try {
+    const { id, name, email, type, age } = req.body;
+    
+    if(!id || !name || !email || !type || !age) {
+        errorCode = 422;
+        throw new Error("Please verify the body content");
+    }
+    const newUser: user = {
+        id,
+        name,
+        email,
+        type,
+        age
+    }
+
+    users.push(newUser);
+    res.status(201).send({message: "User created successfully"});
+        
+    } catch (error) {
+        res.status(errorCode).send({message: error});
+    }
+})
+
+//A - Put para fazer uma especie de update enquanto post para criar um novo.
+
+//b - Put eh mais apropriado para atualizar alguma informacao, como endereco de email, por exemplo.
