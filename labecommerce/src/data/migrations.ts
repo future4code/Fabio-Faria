2d666ab97206b27b9aa85594adbadddff8019f87
fabiofa87 <fabiofadev87@gmail.com>
2021-12-02 18:07:08 -0300

    labecommerce-projeto

---
+++ labecommerce/src/data/migrations.ts
@@ -0,0 +1,26 @@
+import {connection} from '../connections'
+import users from '../users.json'
+
+const printError = (error: any) => { console.log(error.sqlMessage || error.message) }
+
+const createTable = () => connection.raw(`
+      CREATE TABLE IF NOT EXISTS aula51_address (
+        id VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+      );
+   `)
+.then(() => console.log('Table created'))
+.catch(printError);
+
+const insertUsers = () => connection('labecommerce_users')
+.insert(users)
+.then(() => console.log('Users inserted'))
+.catch(printError);
+
+const closeConnection = () => connection.destroy()
+
+createTable()
+.then(insertUsers)
+.then(closeConnection)
